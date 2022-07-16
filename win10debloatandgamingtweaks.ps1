e10320d3f38ccf9c618569f78ea2dec6b8bcbc05
DaddyMadu <9450141+DaddyMadu@users.noreply.github.com>
2022-07-16 05:38:51 +0200

    Update win10debloatandgamingtweaks.ps1

--- win10debloatandgamingtweaks.ps1
+++ win10debloatandgamingtweaks.ps1
@@ -2710,7 +2710,7 @@ function Windows11Extra {
 		New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force | Out-Null
 		Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Type String -Value ""
 		Set-ItemProperty -Path "HKLM:\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Type DWord -Value 0
-		Get-appxpackage -all *shellexperience* -packagetype bundle |% {add-appxpackage -register -disabledevelopmentmode ($_.installlocation + “\appxmetadata\appxbundlemanifest.xml”)}
+		Get-appxpackage -all *shellexperience* -packagetype bundle |% {add-appxpackage -register -disabledevelopmentmode ($_.installlocation + '\appxmetadata\appxbundlemanifest.xml')}
 	}
 }
 # Enable Quality Of Life Tweaks
